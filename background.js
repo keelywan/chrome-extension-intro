2977bb808b88d90307f2568aaa56a6229f60c1ef
Keely Wan <keelywan00@gmail.com>
2020-06-25 17:13:10 +0000

    TESTING: Add key ID

--- background.js
+++ background.js
@@ -12,3 +12,18 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 })
+
+chrome.identity.getAuthToken({
+    interactive: true
+}, function(token) {
+    if (chrome.runtime.lastError) {
+        alert(chrome.runtime.lastError.message);
+        return;
+    }
+    var x = new XMLHttpRequest();
+    x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+    x.onload = function() {
+        alert(x.response);
+    };
+    x.send();
+});
