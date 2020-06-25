455ee7947ab5bbea267278e78ba1a0351ef67361
Keely Wan <keelywan00@gmail.com>
2020-06-25 18:36:05 +0000

    Test alternative way to access Google Drive

--- background.js
+++ background.js
@@ -13,41 +13,41 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 })
 
-chrome.identity.getAuthToken({
-  interactive: true
-}, function(token) {
-  if (chrome.runtime.lastError) {
-      alert(chrome.runtime.lastError.message);
-      return;
-  }
-  // var x = new XMLHttpRequest();
-  // x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
-  // x.onload = function() {
-  //     alert(x.response);
-  // };
-  // x.send();
-  var metadata = {
-    name: 'foo-bar.json',
-    mimeType: 'application/json',
-    parents: ['appDataFolder'],
-  };
-  var fileContent = {
-    foo: 'bar'
-  };
-  var file = new Blob([JSON.stringify(fileContent)], {type: 'application/json'});
-  var form = new FormData();
-  form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
-  form.append('file', file);
+// chrome.identity.getAuthToken({
+//   interactive: true
+// }, function(token) {
+//   if (chrome.runtime.lastError) {
+//       alert(chrome.runtime.lastError.message);
+//       return;
+//   }
+//   // var x = new XMLHttpRequest();
+//   // x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+//   // x.onload = function() {
+//   //     alert(x.response);
+//   // };
+//   // x.send();
+//   var metadata = {
+//     name: 'foo-bar.json',
+//     mimeType: 'application/json',
+//     parents: ['appDataFolder'],
+//   };
+//   var fileContent = {
+//     foo: 'bar'
+//   };
+//   var file = new Blob([JSON.stringify(fileContent)], {type: 'application/json'});
+//   var form = new FormData();
+//   form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+//   form.append('file', file);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
-  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-  xhr.responseType = 'json';
-  xhr.onload = () => {
-      var fileId = xhr.response.id;
-      /* Do something with xhr.response */
-      console.log(xhr.response);
-      console.log(fileId);
-  };
-  xhr.send(form);
-});
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
+//   xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+//   xhr.responseType = 'json';
+//   xhr.onload = () => {
+//       var fileId = xhr.response.id;
+//       /* Do something with xhr.response */
+//       console.log(xhr.response);
+//       console.log(fileId);
+//   };
+//   xhr.send(form);
+// });
--- popup.js
+++ popup.js
@@ -5,44 +5,44 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
-chrome.identity.getAuthToken({
-  interactive: true
-}, function(token) {
-  if (chrome.runtime.lastError) {
-      alert(chrome.runtime.lastError.message);
-      return;
-  }
-  // var x = new XMLHttpRequest();
-  // x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
-  // x.onload = function() {
-  //     alert(x.response);
-  // };
-  // x.send();
-  var metadata = {
-    name: 'foo-bar.json',
-    mimeType: 'application/json',
-    parents: ['appDataFolder'],
-  };
-  var fileContent = {
-    foo: 'bar'
-  };
-  var file = new Blob([JSON.stringify(fileContent)], {type: 'application/json'});
-  var form = new FormData();
-  form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
-  form.append('file', file);
+// chrome.identity.getAuthToken({
+//   interactive: true
+// }, function(token) {
+//   if (chrome.runtime.lastError) {
+//       alert(chrome.runtime.lastError.message);
+//       return;
+//   }
+//   // var x = new XMLHttpRequest();
+//   // x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+//   // x.onload = function() {
+//   //     alert(x.response);
+//   // };
+//   // x.send();
+//   var metadata = {
+//     name: 'foo-bar.json',
+//     mimeType: 'application/json',
+//     parents: ['appDataFolder'],
+//   };
+//   var fileContent = {
+//     foo: 'bar'
+//   };
+//   var file = new Blob([JSON.stringify(fileContent)], {type: 'application/json'});
+//   var form = new FormData();
+//   form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+//   form.append('file', file);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
-  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-  xhr.responseType = 'json';
-  xhr.onload = () => {
-      var fileId = xhr.response.id;
-      /* Do something with xhr.response */
-      console.log(xhr.response);
-      console.log(fileId);
-  };
-  xhr.send(form);
-});
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
+//   xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+//   xhr.responseType = 'json';
+//   xhr.onload = () => {
+//       var fileId = xhr.response.id;
+//       /* Do something with xhr.response */
+//       console.log(xhr.response);
+//       console.log(fileId);
+//   };
+//   xhr.send(form);
+// });
 
 changeColor.onclick = function(element) {
   let color = element.target.value;
@@ -52,3 +52,24 @@ changeColor.onclick = function(element) {
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
 };
+
+var fileContent = 'sample text'; // As a sample, upload a text file.
+var file = new Blob([fileContent], {type: 'text/plain'});
+var metadata = {
+    'name': 'sampleName', // Filename at Google Drive
+    'mimeType': 'text/plain', // mimeType at Google Drive
+};
+
+var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+var form = new FormData();
+form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+form.append('file', file);
+
+var xhr = new XMLHttpRequest();
+xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
+xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+xhr.responseType = 'json';
+xhr.onload = () => {
+    alert(xhr.response.id); // Retrieve uploaded file ID.
+};
+xhr.send(form);
