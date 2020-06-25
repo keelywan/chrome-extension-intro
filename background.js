726f7f0318f4291e1646124b075d51619781dea4
Keely Wan <keelywan00@gmail.com>
2020-06-25 17:58:08 +0000

    Add Google Drive access.

--- background.js
+++ background.js
@@ -13,17 +13,17 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 })
 
-chrome.identity.getAuthToken({
-  interactive: true
-}, function(token) {
-  if (chrome.runtime.lastError) {
-      alert(chrome.runtime.lastError.message);
-      return;
-  }
-  var x = new XMLHttpRequest();
-  x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
-  x.onload = function() {
-      alert(x.response);
-  };
-  x.send();
-});
+// chrome.identity.getAuthToken({
+//   interactive: true
+// }, function(token) {
+//   if (chrome.runtime.lastError) {
+//       alert(chrome.runtime.lastError.message);
+//       return;
+//   }
+//   var x = new XMLHttpRequest();
+//   x.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+//   x.onload = function() {
+//       alert(x.response);
+//   };
+//   x.send();
+// });
