bd73f4bf3a766da36b444bbce6c9badcc7e694bb
Keely Wan <keelywan00@gmail.com>
2020-06-29 17:57:18 +0000

    Add client id and scopes to popup:

--- background.js
+++ background.js
@@ -22,16 +22,6 @@ const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v
 const SPREADSHEET_ID = '1jgrHb-aN2WjteuSuzfOQQ_vtMY728zwZzeHjjgbZMAY';
 const SPREADSHEET_TAB_NAME = 'Sheet1';
 
-const CLIENT_ID = "1005120763196-qiv2jflnrg8or8c84q43rtv8cgcphgs7.apps.googleusercontent.com",
-const SCOPES = [
-      "https://www.googleapis.com/auth/userinfo.email", 
-      "https://www.googleapis.com/auth/drive.appdata", 
-      "https://www.googleapis.com/auth/drive.file",
-      "https://www.googleapis.com/auth/spreadsheets",
-      "https://www.googleapis.com/auth/documents.readonly",
-      "https://www.googleapis.com/auth/documents"
-    ]
-
 function onGAPILoad() {
   console.log("function called");
   gapi.client.init({
