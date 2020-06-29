fd473ac4763edb900fe64a9a4c9df85872149e41
Keely Wan <keelywan00@gmail.com>
2020-06-29 18:29:12 +0000

    Add script

--- popup.js
+++ popup.js
@@ -151,14 +151,14 @@ const SCOPES = [
       "https://www.googleapis.com/auth/documents"
     ]
 
-// function onGAPILoad() {
-//   gapi.load('client:auth2', initClient); 
-//   console.log("loaded");
-// }
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient); 
+  console.log("loaded");
+}
 
 let authorizeButton = document.getElementById('authorize_button');
 let signoutButton = document.getElementById('signout_button');
-function onGAPILoad() {
+function initClient() {
   gapi.client.init({
     apiKey: API_KEY,
     clientId: CLIENT_ID,
